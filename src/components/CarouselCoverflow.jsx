c4ba09f6b514c0fc4fc65c6cef0c9a4a54599ba9
hahayz77 <danilo__danilo@hotmail.com>
2023-09-20 21:53:38 -0300

    swiper added

---
+++ src/components/CarouselCoverflow.jsx
@@ -0,0 +1,25 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCoverflow } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import "../styles/swiper.module.css"
+import CardComponent from './CardComponent';
+
+export default function CarouselCoverflow() {
+    return (
+        <>
+            <Swiper slidesPerView={3} spaceBetween={30} modules={[EffectCoverflow]} className="mySwiper" effect={'coverflow'}
+                coverflowEffect={{ rotate: 0, stretch: 0, depth: 100, modifier: 1, slideShadows: true, }} onSlideChange={slide => console.log(slide.activeIndex)}>
+                <SwiperSlide><CardComponent /></SwiperSlide>
+                <SwiperSlide><CardComponent /></SwiperSlide>
+                <SwiperSlide><CardComponent /></SwiperSlide>
+                <SwiperSlide><CardComponent /></SwiperSlide>
+                <SwiperSlide><CardComponent /></SwiperSlide>
+                <SwiperSlide><CardComponent /></SwiperSlide>
+                <SwiperSlide><CardComponent /></SwiperSlide>
+                <SwiperSlide><CardComponent /></SwiperSlide>
+                <SwiperSlide><CardComponent /></SwiperSlide>
+            </Swiper>
+        </>
+    );
+}
