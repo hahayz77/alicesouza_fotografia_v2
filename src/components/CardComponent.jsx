d3691cc9b11c1b4b70e32bf45575705fdf6ce1b1
hahayz77 <danilo__danilo@hotmail.com>
2023-09-30 10:35:32 -0300

    cardComponent css corrections + dark theme

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -6,27 +6,22 @@ import { urlForImage } from "../../sanity/lib/image";
 export default function CardComponent({ name, print, profile, text, mandacaru }) {
     return (
         <>
-            <Card className={`py-4 max-w-sm overflow-visible my-24 ${mandacaru ? null : "scale-80" }`}>
-                {mandacaru && <figure className="object-cover w-full top-0 right-0 bg-green-300 translate-y-[-2rem] translate-x-[40%]">
-                    <Image
-                        alt="Card background"
-                        className="object-cover rounded-full object-center h-24 w-24 scale-125 absolute -bottom-6 left-0 translate-y-[50%] translate-x-[8.5rem] z-[0]"
-                        src="/img/mandacaru.png"
-                        width={100}
-                    />
-                </figure>}
-                <CardHeader className="pb-0 pt-2 px-4 flex-col items-start overflow-visible">
-                    <RiDoubleQuotesL className="font-serif text-[8rem] translate-y-[-20%] absolute opacity-20" />
-                    <h4 className="font-bold text-large text-center w-full my-6">{name}</h4>
+            <Card className={`testimonial_card ${mandacaru ? null : "scale-80"}`}>
+                {mandacaru &&
+                    <figure className="testimonial_mandacaru">
+                        <Image alt="Card background" src="/img/mandacaru.png" width={100} />
+                    </figure>}
+                <CardHeader className="card_header">
+                    <RiDoubleQuotesL className="quotes" />
+                    <h4>{name}</h4>
                 </CardHeader>
-                <CardBody className="overflow-visible py-2 px-10 mb-12 text-justify">
+                <CardBody className="card_body">
                     <p>{text.length > 150 ? text.slice(0, 150) + "..." : text}</p>
-                    <br /> <hr className="w-[30%] mx-auto" /> <br />
-                    <p className="text-center font-light text-slate-500 cursor-pointer" onClick={() => { }}>Ver depoimento completo</p>
+                    <br /> <hr /> <br />
+                    <p className="p_link" onClick={() => { }}>Ver depoimento completo</p>
                 </CardBody>
-                <figure className="object-cover w-full inset-0">
-                    <Image alt="Card background" src={urlForImage(profile).url()} width={100}
-                        className="object-cover rounded-full object-center h-24 w-24 scale-125 absolute -bottom-6 left-0 translate-y-[50%] translate-x-[8.5rem] z-[0] border-1 border-slate-400" />
+                <figure className="testimonial_profile">
+                    <Image alt="Card background" src={urlForImage(profile).url()} width={100} />
                 </figure>
 
             </Card>
