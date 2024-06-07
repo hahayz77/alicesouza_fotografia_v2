6cab093cb6cbc7d8fc5d4516a9b05cadc9668c50
Fialho <danilo__danilo@hotmail.com>
2024-06-06 23:10:24 -0300

    Fix pathname condition to studio path

--- src/components/sections/Nav.jsx
+++ src/components/sections/Nav.jsx
@@ -2,24 +2,27 @@ import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from 'next/navigation'
 import { HiMenu } from 'react-icons/hi';
+import MenuMobile from "../MenuMobile";
 
 const Nav = () => {
 
-    const pathname = usePathname();
+    const pathname = usePathname()
+    const isStudioPath = () => pathname.includes('/studio')
 
     return (
         <>
-            {pathname.slice(0, 7) !== "/studio" ?
-                (<nav id="nav">
-                    <Link href={"#"} className="icon_container">
-                        <HiMenu style={{ color: '#ffffff', fontSize: '1.8rem' }} />
-                    </Link>
-                    <Link href={"/"} className="logo_container">
-                        <figure> <Image height={100} width={170} src={"/img/logo.png"} alt="logo" /> </figure>
-                    </Link>
-                </nav>)
-                :
-                null
+            {!isStudioPath() &&
+                <>
+                    <nav id="nav">
+                        <Link href={"#"} className="icon_container">
+                            <HiMenu style={{ color: '#ffffff', fontSize: '1.8rem' }} />
+                        </Link>
+                        <Link href={"/"} className="logo_container">
+                            <figure> <Image height={100} width={170} src={"/img/logo.png"} alt="logo" /> </figure>
+                        </Link>
+                    </nav>
+                    {/* <MenuMobile /> */}
+                </>
             }
         </>
     )
